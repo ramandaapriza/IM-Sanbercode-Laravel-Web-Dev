551ae0c34c4a8196d4cd0dd1fbc9800f606c0e1d
ramandaapriza <ramanda192017@gmail.com>
2024-03-01 09:04:25 +0700

    menambahkan tugas OOP PHP hari - 9

---
+++ oop/Ape.php
@@ -0,0 +1,15 @@
+<?php
+
+   require_once('animal.php');
+
+      class Ape extends Animal
+      {
+            public $legs = 2;
+
+            public function yell()
+            {
+                  echo "Auooo\n";
+               }
+      }
+
+ ?>
---
+++ oop/Frog.php
@@ -0,0 +1,14 @@
+<?php
+
+      require_once('animal.php');
+
+      class Frog extends Animal
+      {
+         public function jump()
+         {
+              echo "Hop Hop\n";
+         }
+       }
+
+ ?>
+
